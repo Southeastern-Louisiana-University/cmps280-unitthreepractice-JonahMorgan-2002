086f37a9bdc7288761de39c51d4b1e49259041a0
W0729503 <jonah.morgan@selu.edu>
2023-10-16 15:16:59 -0500

    10/16/23

--- Quotient.java
+++ Quotient.java
@@ -10,8 +10,21 @@ public class Quotient {
     System.out.print("Enter two integers: ");
     int number1 = input.nextInt();
     int number2 = input.nextInt();
-    
-    System.out.println(number1 + " / " + number2 + " is " +
-      (number1 / number2));
+
+    if (number2 != 0)
+      System.out.println(number1 + " / " + number2 + " is " + (number1 / number2));
+    else
+      System.out.println("Divisor cannot be 0");
+    int result = quotient(number1, number2);
+    System.out.println("Value of quotient is " + result);
+
+  }
+
+  public static int quotient(int number1, int number2){
+    if (number2 == 0){
+      System.out.println("Divisor cannot be 0.");
+      System.exit(1);
+    }
+    return (number1 / number2);
   }
 }
